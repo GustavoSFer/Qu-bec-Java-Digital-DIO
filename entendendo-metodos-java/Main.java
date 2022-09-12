4dcbe93caf10d987e1f1d4477c5e02c625c4fe67
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-09-12 15:53:28 -0300

    Criando uma classe Calculadora para fazer as operações basicas

---
+++ entendendo-metodos-java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Calculadora.soma(1,4);
+    Calculadora.subtracao(4, 1);
+    Calculadora.multiplicacao(2, 12);
+    Calculadora.divisão(10, 5);
+  }
+}
