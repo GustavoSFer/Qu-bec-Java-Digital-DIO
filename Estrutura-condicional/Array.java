a6de5a0c9cbdbd218865ecc83ab17070087ed7e2
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-09-14 17:17:43 -0300

    percorrendo um array com um for

---
+++ Estrutura-condicional/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+  public static void main(String[] args) {
+    String nomes[] = { "pedro", "João", "Aline", "Pamela" };
+
+    for (int i = 0; i < nomes.length; i ++) {
+      System.out.println("A posição do '" + nomes[i] + "' é: " + i);
+    }
+  }
+}
