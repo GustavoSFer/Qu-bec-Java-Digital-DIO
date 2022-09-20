5d0c2b81181da8de7540bbb730c6b1e7b2365618
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-09-20 15:26:13 -0300

    Exercicio 2 - criando uma classe veiculo

---
+++ introducao-programacao-orientado-a-objeto/Exercicio2/Veiculo.java
@@ -0,0 +1,32 @@
+package Exercicio2;
+
+public class Veiculo {
+  String modelo;
+  String cor;
+  Integer capacidadeTanque;
+
+  Veiculo() {};
+  Veiculo(String modelo, String cor, Integer capacidadeTanque) {
+    this.modelo = modelo;
+    this.cor = cor;
+    this.capacidadeTanque = capacidadeTanque;
+  };
+
+  void setModelo(String modelo) {
+    this.modelo = modelo;
+  };
+  String getModelo() {
+    return this.modelo;
+  };
+
+  void setCor(String cor) {
+    this.cor = cor;
+  };
+  String getCor() {
+    return this.cor;
+  };
+
+  void setCapacidadeTanque(Integer capacidade) {
+    this.capacidadeTanque = capacidade;
+  };
+}
