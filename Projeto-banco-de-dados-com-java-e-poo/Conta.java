52acd0d9bbd5ec43f4e4e1a64c664ef978a5aff2
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-10-03 14:34:40 -0300

    implementando nossos metodos

--- Projeto-banco-de-dados-com-java-e-poo/Conta.java
+++ Projeto-banco-de-dados-com-java-e-poo/Conta.java
@@ -15,20 +15,18 @@ public abstract class Conta implements IConta {
 
   @Override
   public void sacar(double valor) {
-    // TODO Auto-generated method stub
-    
+    this.saldo -= valor;    
   }
 
   @Override
   public void depositar(double valor) {
-    // TODO Auto-generated method stub
-    
+  this.saldo += valor;    
   }
 
   @Override
   public void transferir(double valor, Conta contaDestino) {
-    // TODO Auto-generated method stub
-    
+    this.sacar(valor);  
+    contaDestino.depositar(valor);
   }
 
   public int getAgencia() {
