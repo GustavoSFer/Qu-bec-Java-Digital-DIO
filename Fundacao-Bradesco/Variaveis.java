99fb1aae0ab1e3a09b2acc295ca7a63b5f0671fa
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-10-05 15:43:21 -0300

    Veriavel em java

---
+++ Fundacao-Bradesco/Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+  public static void main(String[] args) {
+    String nome = "Carlos";
+    int idade = 30;
+    boolean casado = true;
+
+    System.out.println("Bem vindo " + nome + ", você tem " + idade + " anos de idade");
+    System.out.println("Casado? " + casado);
+
+  }
+}
