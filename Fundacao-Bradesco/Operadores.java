c580a14d3215c1f2c0309afc7881c14244bb56e9
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-10-05 16:08:26 -0300

    Operadores basicos

---
+++ Fundacao-Bradesco/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+  public static void main(String[] args) {
+    double a = 7 + 3;
+    System.out.println(a);
+
+    double b = 7 * 3;
+    System.out.println(b);
+
+    double c = 7 - 3;
+    System.out.println(c);
+
+    double d = 10 % 2;
+    System.out.println(d);
+
+  }
+}
